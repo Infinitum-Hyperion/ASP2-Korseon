077b205d265d40946ab9f8e2c8e44eaa82cc1902
ObsidianXVI <siddharth.chitikela@gmail.com>
2025-01-03 08:26:32 +0800

    remove old models

--- korseon/models/lane_segmentation/wickelheim/run.sh
+++
@@ -1,4 +0,0 @@
-# run using: !sh ./run.sh
-gdown --id 1LTdUXzUWcnHuEEAiMoG42oAGuJggPQs8 -O ./data/culane/driver_100_30frame.tar.gz
-tar -xvzf ./data/culane/driver_100_30frame.tar.gz -C ./data/culane/
-python ./train.py --data_dir data/culane --dataset culane --learning_rate 5e-4
